89c43fd6b10e100128ee98176841a8d907622597
Adrien Ronse <adrien.ronse@epitech.eu>
2024-01-15 15:18:17 +0100

    url

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
             targets: ["IterationXFramework"]),
     ],
     targets: [
-        .binaryTarget(name: "IterationXFramework", url: "https://github.com/Iteration-X/IterationXFramework/releases/download/1.0.0/ItxFramework.xcframework.zip", checksum: "d777872a47f18661ae7d757d58947b17bac239f6aee02e1e73d12971a545c8bd")
+        .binaryTarget(name: "IterationXFramework", url: "https://github.com/Iteration-X/IterationXFramework/releases/download/1.0.0/ItxFramework.xcframework.zip", checksum: "779bab6e9e1f1545af34f3db666a1a34f61372e67c22126cd452ba6b6f598e53")
     ]
 )
